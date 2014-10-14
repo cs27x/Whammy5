f7be8075a08ff55a0eb99fb4c3de84f6f292ec3b
Christian Reynolds <christian.b.reynolds@vanderbilt.edu>
2014-10-14 11:20:49 -0500

    My tests

---
+++ src/main/java/org/magnum/cs278/testdriven/SecondSevenTests.java
@@ -0,0 +1,14 @@
+package org.magnum.cs278.testdriven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SecondSevenTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
